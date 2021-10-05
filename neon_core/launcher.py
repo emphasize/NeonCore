b5fcacf41ee798233fd09be2128f3d01e23f1522
jarbasal <jarbasai@mailfence.com>
2021-10-06 00:21:50 +0100

    lock

--- neon_core/launcher.py
+++ neon_core/launcher.py
@@ -1,9 +1,11 @@
+from mycroft.lock import Lock
 from mycroft.util import wait_for_exit_signal, reset_sigint_handler
 from neon_core.messagebus.service import NeonBusService
 from neon_core.skills.service import NeonSkillService
 
 reset_sigint_handler()
 # Create PID file, prevent multiple instances of this service
+# TODO should also detect old services Locks
 lock = Lock("NeonCore")
 
 # launch websocket listener
