1b420442668402fce7314567d49feb1e52740920
jarbasal <jarbasai@mailfence.com>
2021-10-06 00:20:38 +0100

    fix imports

--- neon_core/launcher.py
+++ neon_core/launcher.py
@@ -1,8 +1,6 @@
-from mycroft.messagebus.load_config import load_message_bus_config
 from mycroft.util import wait_for_exit_signal, reset_sigint_handler
-from neon_core.messagebus.service import create_websocket_app
-from neon_core.skills.service import NeonSkillService
 from neon_core.messagebus.service import NeonBusService
+from neon_core.skills.service import NeonSkillService
 
 reset_sigint_handler()
 # Create PID file, prevent multiple instances of this service
