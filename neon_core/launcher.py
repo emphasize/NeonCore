cc523fe6a0a365d2b1288d0489f02dd925803f1f
jarbasal <jarbasai@mailfence.com>
2021-10-06 01:49:16 +0100

    refactor gui service

--- neon_core/launcher.py
+++ neon_core/launcher.py
@@ -2,6 +2,7 @@ from mycroft.lock import Lock
 from mycroft.util import wait_for_exit_signal, reset_sigint_handler
 from neon_core.messagebus.service import NeonBusService
 from neon_core.skills.service import NeonSkillService
+from neon_core.gui.service import NeonGUIService
 from time import sleep
 
 reset_sigint_handler()
@@ -13,6 +14,10 @@ lock = Lock("NeonCore")
 bus = NeonBusService(daemonic=True)
 bus.start()
 
+# launch GUI websocket listener
+gui = NeonGUIService(daemonic=True)
+gui.start()
+
 # launch skills service
 skills = NeonSkillService()
 skills.start()
@@ -20,4 +25,5 @@ skills.start()
 wait_for_exit_signal()
 
 skills.shutdown()
+gui.shutdown()
 bus.shutdown()
